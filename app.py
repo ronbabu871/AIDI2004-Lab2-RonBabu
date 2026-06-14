1e70392f1c402eb1dca6749c13ecda3f2d443770
Ron Babu <ronbabu871@gmail.com>
2026-06-14 17:08:30 -0400

    Update greeting message - Ron Babu

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py - Created by Ron Babu
 
 def greet(name):
-    return f"Hello, {name}! Welcome to AIDI 2004."
+ return f"Hi, {name}! GitHub Actions is working."
 
 if __name__ == "__main__":
     print(greet("Ron"))
